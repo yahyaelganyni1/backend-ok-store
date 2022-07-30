a8631516e8cf42e60df62ea113a68aafa7153963
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-07-30 22:01:14 +0200

    Add validates to the user model

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,10 +12,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_success
-    render json: { message: 'Signed up sucessfully.' }
+    render json: { message: 'Signed up sucessfully.', user: resource }, status: :ok
   end
 
   def register_failed
     render json: { message: "Something went wrong." }
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          :registerable,
          jwt_revocation_strategy: JwtDenylist
+
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 end
