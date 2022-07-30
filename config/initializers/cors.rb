e2ad6376d1833f827d02704ff95f70ac54f6675e
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-07-30 21:26:45 +0200

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,3 @@
-# Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
@@ -10,7 +9,20 @@
 #     origins "example.com"
 #
 #     resource "*",
+# Be sure to restart your server when you modify this file.
 #       headers: :any,
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource(
+       '*',
+       headers: :any,
+       expose: ["Authorization"],
+       methods: [:get, :patch, :put, :delete, :post, :options, :show]
+      )
+    end
+end
